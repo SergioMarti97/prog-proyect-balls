20a8a863f5601cbafe02264e422f9c81ab53ee44
Sergio Martí Torregrosa <sergio.martihee1997@gmail.com>
2020-07-10 21:13:01 +0200

    Cambio de última hora

--- src/engine/gfx/shapes2d/QuickMath.java
+++ src/engine/gfx/shapes2d/QuickMath.java
@@ -4,7 +4,7 @@ package engine.gfx.shapes2d;
 * todo: revisar en Java que operaciones con números en coma flotante son más rapidas. Si float, si double o extended.
 *  Si mi programa es de 32Bits (son ejecutables de 32 bits) el más rápido son double. Pero extended tiene
 *  nativo el co-procesador matemático.
-*  He probado a substutir a Math.sin y .cos por esta clase y va más lento y además pasamos de rotación a escalado?
+*  He probado a substutir a Math.sin y .cos por esta clase y va más lento
 */
 
 public class QuickMath {
@@ -21,7 +21,7 @@ public class QuickMath {
         coss = new double[DEGREES];
         for ( int i = 0; i < DEGREES; i++ ) {
             sins[i] = Math.sin(i * pi / 180.0);
-            sins[i] = Math.cos(i * pi / 180.0);
+            coss[i] = Math.cos(i * pi / 180.0);
         }
     }
 
